33199526b8d1b8fea0c7e8878528513a0bcf3bbf
silvana02 <silvanalima.89@hotmail.com>
2023-12-14 14:58:10 +0100

    [FEAT] Method override and body parsers

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const methodOverride = require("method-override");
 const mainRoutes = require("./src/routes/mainRoutes");
 const shopRoutes = require("./src/routes/shopRoutes");
 
 app.use(express.static("public"));
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(methodOverride("_method"));
 
 app.use("/", mainRoutes);
 app.use("/shop", shopRoutes);
